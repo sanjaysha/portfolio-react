a6e4d1e9754410863d91bd8fd623cd7f5fbeb9ab
Sanjay Singh Shahi <shahi.sanjayshahi@gmail.com>
2025-12-09 13:53:46 +0530

    nvmrc added for deployment to use node 20 and fixed file names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
